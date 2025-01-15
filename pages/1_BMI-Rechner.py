ee2b9cf86a8cd3d9ab9149bc89db5b87fe80863d
Samuel Wehrli <samuel.wehrli@gmail.com>
2025-01-15 19:00:09 +0100

    Introduced st.form

--- pages/1_BMI-Rechner.py
+++ pages/1_BMI-Rechner.py
@@ -9,13 +9,16 @@ app_manager.login_page()
 
 st.title('BMI Rechner')
 
-# Get user input for height and weight
-height = st.number_input('Geben Sie Ihre Größe ein (in Meter)', min_value=0.1, max_value=3.0, value=1.7, step=0.01)
-weight = st.number_input('Geben Sie Ihr Gewicht ein (in kg)', min_value=1.0, max_value=500.0, value=70.0, step=0.1)
+with st.form("BMI Eingabeformular"):
+    # Get user input for height and weight
+    height = st.number_input('Geben Sie Ihre Größe ein (in Meter)', min_value=0.1, max_value=3.0, value=1.7, step=0.01)
+    weight = st.number_input('Geben Sie Ihr Gewicht ein (in kg)', min_value=1.0, max_value=500.0, value=70.0, step=0.1)
 
-# Calculate BMI when button is clicked
-if st.button('BMI berechnen'):
-
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    
+if submitted:
+    
     result = calculate_bmi(height, weight)
     
     st.write(f'Ihr BMI ist: {result["bmi"]}')
